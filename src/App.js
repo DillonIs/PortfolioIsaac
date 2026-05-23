dc50118f5fc027fcb5a2a1091f58e7ce81f4220a
Bree Kagwe <kagwe.bridgette@gmail.com>
2026-05-23 14:57:09 +1000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ import Home from "./components/Home/Home";
 import Projects from "./components/Projects/Projects";
 import Footer from "./components/Footer";
 import Resume from "./components/Resume/ResumeNew";
+import RedTeam1 from "./components/Projects/Writeups/RedTeam1";
+import RedTeam2 from "./components/Projects/Writeups/RedTeam2";
+import RedTeam3 from "./components/Projects/Writeups/RedTeam3";
+import BlueTeam1 from "./components/Projects/Writeups/BlueTeam1";
+import BlueTeam2 from "./components/Projects/Writeups/BlueTeam2";
+import BlueTeam3 from "./components/Projects/Writeups/BlueTeam3";
 import {
   BrowserRouter as Router,
   Route,
@@ -37,6 +43,12 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/project" element={<Projects />} />
           <Route path="/resume" element={<Resume />} />
+          <Route path="/writeup/redteam1" element={<RedTeam1 />} />
+          <Route path="/writeup/redteam2" element={<RedTeam2 />} />
+          <Route path="/writeup/redteam3" element={<RedTeam3 />} />
+          <Route path="/writeup/blueteam1" element={<BlueTeam1 />} />
+          <Route path="/writeup/blueteam2" element={<BlueTeam2 />} />
+          <Route path="/writeup/blueteam3" element={<BlueTeam3 />} />
           <Route path="*" element={<Navigate to="/"/>} />
         </Routes>
         <Footer />
